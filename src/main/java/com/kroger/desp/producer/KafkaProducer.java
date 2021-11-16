a41f9d3a787f053983bffd244a63ecf8161909ca
kon4929 <rob.hoeting@kroger.com>
2021-11-16 08:33:22 -0500

    removed uneeded code

--- src/main/java/com/kroger/desp/producer/KafkaProducer.java
+++ src/main/java/com/kroger/desp/producer/KafkaProducer.java
@@ -2,17 +2,13 @@ package com.kroger.desp.producer;
 import com.kroger.desp.events.despelmr.test.BadThingHappened;
 import com.kroger.desp.events.despelmr.test.CoolThingHappened;
 import com.kroger.desp.events.despelmr.test.TestEvent;
-import org.apache.kafka.clients.producer.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Component
 public class KafkaProducer {
@@ -35,8 +31,6 @@ public class KafkaProducer {
     @Autowired
     TestEventCreator testEventCreator;
 
-    @Autowired
-    HappenedEventCreator happenedEventCreator;
 
     @Value( "${target-topic}" )
     private String targetTopic;
@@ -49,23 +43,4 @@ public class KafkaProducer {
         publisher.send(targetTopic, event.getEventHeader().getId(), event);
     }
 
-    @Autowired
-    KafkaTemplate<String, CoolThingHappened> coolThingHappenedKafkaTemplate;
-
-    public void publishCoolThingHappened(int i){
-        logger.info("Sending Cool Thing EVENT #" + i + " To topic: " + targetTopic);
-        CoolThingHappened event = happenedEventCreator.generateCoolThingHappenedEvent().build();
-        final RetryingEventPublisher<CoolThingHappened> publisher = coolThingPublisherProvider.getObject();
-        publisher.send(targetTopic, event.getEventHeader().getId(), event);
-    }
-
-    @Autowired
-    KafkaTemplate<String, BadThingHappened> badThingHappenedKafkaTemplate;
-
-    public void publishBadThingHappened(int i){
-        logger.info("Sending Bad Thing EVENT #" + i + " To topic: " + targetTopic);
-        BadThingHappened event = happenedEventCreator.generateBadThingHappenedEvent().build();
-        final RetryingEventPublisher<BadThingHappened> publisher = badThingPublisherProvider.getObject();
-        publisher.send(targetTopic, event.getEventHeader().getId(), event);
-    }
 }
